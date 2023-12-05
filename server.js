f7197158734f0f1338b1ffd54ee0cf097689af8a
Endo Axelsson <endo.axelsson@elev.ga.ntig.se>
2023-12-05 11:02:44 +0100

    🎉

---
+++ server.js
@@ -0,0 +1,27 @@
+const nunjucks = require(`nunjucks`)
+const express = require ("express")
+const indexRouter = require ("./routes/index")
+
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+})
+
+app.use("/", indexRouter)
+
+app.use(express.static(`public`))
+
+app.get("/", (request, response) => {
+    response.render('index.njk', { title: 'hello from template'})
+})
+
+
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
